f9757dc8879ca8e0d91d0ecc0b2f2ad719c4328e
angappan-m <78061914+angappan-m@users.noreply.github.com>
2021-03-21 19:35:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(bodyParser.json())
 app.use('/api',apiRoutes);
 
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
     console.log('application on http://localhost:'+process.env.PORT+'/api');
 });
