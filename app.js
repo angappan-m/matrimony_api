f9fe48f65244d231f0ff39373591b196fb71a96e
angappan-m <78061914+angappan-m@users.noreply.github.com>
2021-03-24 19:50:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const apiRoutes = require('./routes/userRoute');
+require('dotenv/config');
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+});
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/api',apiRoutes);
+
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('application on http://localhost:'+process.env.PORT+'/api');
+});
