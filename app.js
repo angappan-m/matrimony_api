3530f4610852632da1c04bac49594a56a35f835d
angappan-m <78061914+angappan-m@users.noreply.github.com>
2021-03-27 09:05:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const apiRoutes = require('./routes/userRoute');
 require('dotenv/config');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const cors = require('cors');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+app.use(cors());
 
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-});
-
-// parse application/json
-app.use(bodyParser.json())
+// app.use((req,res,next) => {
+//     res.header('Access-Control-Allow-Origin','*');
+//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     if(req.method === 'OPTIONS'){
+//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 app.use('/api',apiRoutes);
 
